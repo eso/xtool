f359ad040cf26a0272d089fd81f677c7a1f28f21
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2016-04-04 18:06:17 +0200

    additions to xtool model

--- xtool/model.py
+++ xtool/model.py
@@ -1,8 +1,16 @@
 class InstrumentModel(object):
     pass
 
-class XShooterInstrumentModel(object):
-    pass
+class XShooterInstrumentModel(InstrumentModel):
 
-class PolynomialXShooterInstrumentModel(object):
+    data_fname = 'SCI_SLIT_DIVFF_NIR.fits'
+
+    transform_pixel_to_wave_fname = 'SCI_SLIT_WAVE_MAP_NIR.fits'
+
+    def read_reduced_data(self, data_directory):
+
+        pass
+
+
+class PolynomialXShooterInstrumentModel(XShooterInstrumentModel):
     pass
